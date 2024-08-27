d5de5b727e8df40b9ddb41ec861a26385162670f
luisp <luisfelipe_pellis@hotmail.com>
2024-08-26 21:58:50 -0300

    atualizando dados da aprendizagem de tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos Primitivos
+        // Estudar sobre a classe String que representa texto na aplicação 
+
+        String meuNome = "Luis Pellis";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; 
+
+        int numero = 5;
+
+        // Exemplos
+
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
     }
 }
