7a907c7c30c814b16435fd16060e8a267ecb520e
luisp <luisfelipe_pellis@hotmail.com>
2024-09-22 19:22:39 -0300

    atualizando o aprendizado de Estrutura Condicional

--- estrutura-condicional/src/CondicionalSimples/CaixaELetronico.java
+++ estrutura-condicional/src/CondicionalSimples/CaixaELetronico.java
@@ -1,5 +1,10 @@
 public class CaixaELetronico {
 
+    /*
+     * A Estrutura Condicional possibilita a escolha de um grupo de ações e comportamentos a serem executados quando determinadas
+     * condições são ou não satisfeitas. A Estrutura Condicional pode ser Simples ou Composta.s
+     */
+
     public static void main(String[] args) {
         
         double saldo = 25.0;
---
+++ estrutura-condicional/src/CondicionalSimples/ResultadoEscolar.java
@@ -0,0 +1,18 @@
+public class ResultadoEscolar {
+
+    /* Algumas vezes o nosso programa deverá seguir mais de uma jornada de execução condicionado a uma regra de negócio, este cenário
+     * é denominado Estrutura Condicional Composta. Exemplo abaixo: 
+     */
+    
+    public static void main(String[] args) {
+        
+         int nota = 6;
+
+         if(nota >= 7 )
+            System.out.println("Aprovado!");
+
+        else 
+            System.out.println("Reprovado!");
+    }
+
+}
