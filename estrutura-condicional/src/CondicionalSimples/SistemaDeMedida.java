75ce76b6a62a47e9357c38561aa79252e4f35db9
luisp <luisfelipe_pellis@hotmail.com>
2024-09-23 09:22:47 -0300

    atualizando o aprendizado de switch

---
+++ estrutura-condicional/src/CondicionalSimples/SistemaDeMedida.java
@@ -0,0 +1,27 @@
+public class SistemaDeMedida {
+
+    public static void main(String[] args) {
+        
+        /*
+         * A Estrutura Switch compara o valor de cada caso com o da variavel sequencialmente, e sempre que encontra um valor corres-
+         * pondente, executa o código associado ao caso. Para evitar que as comparações continuem a ser executadas após um caso 
+         * correspondente ter sido encontrado, acrescentamos o comando BREAK no final de cada bloco de códigos. O comando BREAK 
+         * quando executado, encerra a execução da estrutura onde ele se encontra. 
+         * 
+         * Vamos imaginar que precisaremos imprimir uma medida com base em mapa de valores, abaixo:
+         * _________________________________________________________
+         * |                                                        |
+         * |         Sigla               |           Tamanho        |
+         * |                             |                          |
+         * |          P                  |           PEQUENO        |
+         * |          M                  |           MEDIO          |
+         * |          G                  |           GRANDE         |
+         * |________________________________________________________|
+         * 
+         */
+
+         
+
+    }
+    
+}
